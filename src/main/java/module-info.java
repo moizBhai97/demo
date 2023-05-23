2610e7ecb280a087d867b63db7fd8603ab6cd888
moizBhai97 <moizrootsdha@gmail.com>
2023-05-23 15:10:52 +0500

    Commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,6 @@ module com.example {
     requires java.sql;
     requires com.microsoft.sqlserver.jdbc;
 
-    opens com.example to javafx.fxml;
+    opens com.example.UIController to javafx.fxml;
     exports com.example;
 }
