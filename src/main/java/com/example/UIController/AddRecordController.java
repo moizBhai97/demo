484f36b976658f2522e46d87086c3c1084a0a778
Musa <musaharoon.2003@gmail.com>
2023-05-28 19:53:38 +0500

    musa

---
+++ src/main/java/com/example/UIController/AddRecordController.java
@@ -0,0 +1,20 @@
+package com.example.UIController;
+
+import java.lang.String;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class AddRecordController {
+
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField descriptionTextBox;
+    @FXML
+    private ComboBox<?> typeComboBox;
+
+    
+}
