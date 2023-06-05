8a99116d6235e0919a23243b25d6b9ee7548248e
M-Abdullah <m18ab03@gmail.com>
2023-06-05 22:24:58 +0500

    asd

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -305,6 +305,7 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+    
     public static void main(String[] args) {
         launch();
         System.out.println("Gay world");
